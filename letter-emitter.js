e4d49d88c30327b6c32d364b59ff98e9fe2f5fc9
Lili Boxer <ninaliboxer@gmail.com>
2019-07-09 16:30:09 -0700

    all working

--- letter-emitter.js
+++ letter-emitter.js
@@ -2,12 +2,21 @@ const EventListener = require('events');
 
 class LetterEmitter extends EventListener {
   read(str) {
+    console.log(str);
     str
       .split('')
-      .forEach(item => {
-        this.emit()
+      .forEach((item, index) => {
+        // 2. publish
+        // where we are creating the content of letterObj
+        this.emit('onLetterObj', {
+          // letter is naming the event 
+          letter: item,
+          offset: index
+        })
       })
+    this.emit('end');
   }
 }
+
 
 module.exports = { LetterEmitter };
--- letter-emitter.test.js
+++ letter-emitter.test.js
@@ -1,16 +1,20 @@
 const {LetterEmitter} = require('./letter-emitter');
 
-describe('letter emitter', done => {
+describe('letter emitter', () => {
   let letterEmitter;
+
   beforeEach(done => {
     letterEmitter = new LetterEmitter();
     done();
   })
-  it('splits string', () => {
+
+  it('splits string', done => {
     const str = 'Max is the best kitty';
-    // letterEmitter = new LetterEmitter;
-    letterEmitter.on('item', letter => {
-      expect(str).toContainEqual(letter)
+    // 1. subscribe 
+    // listening for emitter and is creating letterObj
+    letterEmitter.on('item', letterObj => {
+      // 3. checking that the letter will match the index
+      expect(str[letterObj.offset]).toEqual(letterObj.letter)
     })
     letterEmitter.on('end', () => {
       done();
